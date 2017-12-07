f67732971c9294a921d09990c1abb253e8866242
HoHooHooo <elijah.hehao@gmail.com>
2017-12-07 23:52:23 +0800

    页面布局

--- src/welcome.jsx
+++ src/welcome.jsx
@@ -45,7 +45,7 @@ class WelcomePage extends React.Component {
                                 <FormItem>
                                     {getFieldDecorator('userName', {
                                         rules: [{ required: true, message: 'Please input your username!' }],
-                                    })(
+                                    })(  
                                         <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
                                         )}
                                 </FormItem>
@@ -71,7 +71,7 @@ class WelcomePage extends React.Component {
                                     Or <a href="">register now~!</a>
                                 </div>
 
-                            </Form> : <div onClick={this.toLogin.bind(self)}>
+                            </Form> : <div className='login-form' onClick={this.toLogin.bind(self)}>
                                 请登录
 
                         </div>}
